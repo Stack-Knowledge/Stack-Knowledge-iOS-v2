e1c885ae2a3ecff0b79c59dae02118c44ee0b04d
uuuunseo <jdbstjoo@gmail.com>
2024-02-27 23:50:58 +0900

    :sparkles: :: buttonWrapper 추가

---
+++ App/Sources/DesignSystem/Button/CTAButton/CTAButton.swift
@@ -0,0 +1,9 @@
+//
+//  CTAButton.swift
+//  StackKnowledge
+//
+//  Created by 정윤서 on 2/27/24.
+//  Copyright © 2024 team.filo. All rights reserved.
+//
+
+import Foundation
---
+++ App/Sources/DesignSystem/Extensions/View/SKAlertButtonType.swift
@@ -0,0 +1,9 @@
+//
+//  SKAlertButtonType.swift
+//  StackKnowledge
+//
+//  Created by 정윤서 on 2/27/24.
+//  Copyright © 2024 team.filo. All rights reserved.
+//
+
+import Foundation

56411496236f447f0134e9d5c2a0499993097717
uuuunseo <jdbstjoo@gmail.com>
2024-03-05 17:39:33 +0900

    :sparkles: :: add SKAlert

--- App/Sources/DesignSystem/Extensions/View/SKAlert.swift
+++ App/Sources/DesignSystem/Extensions/View/SKAlert.swift
@@ -1,9 +1,73 @@
-//
-//  SKAlert.swift
-//  StackKnowledge
-//
-//  Created by 정윤서 on 2/27/24.
-//  Copyright © 2024 team.filo. All rights reserved.
-//
+import SwiftUI
 
-import Foundation
+public extension View {
+    func skAlert(
+        title: String,
+        isShowing: Binding<Bool>,
+        alertActions: [SKAlertButtonType]
+    ) -> some View {
+        modifier(
+            SKAlertModifier(
+                title: title,
+                isShowing: isShowing,
+                alertActions: alertActions
+            )
+        )
+    }
+}
+
+struct SKAlertModifier: ViewModifier {
+    var title: String
+    @Binding var isShowing: Bool
+    var alertActions: [SKAlertButtonType]
+
+    public init(
+        title: String,
+        isShowing: Binding<Bool>,
+        alertActions: [SKAlertButtonType]
+    ) {
+        self.title = title
+        _isShowing = isShowing
+        self.alertActions = alertActions
+    }
+
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+
+            ConditionView(isShowing) {
+                Color.sk(.gray(.darkgray3))
+                    .opacity(0.25)
+                    .ignoresSafeArea()
+
+                skAlert()
+                    .padding(40)
+                    .transition(
+                        .asymmetric(
+                            insertion: AnyTransition.move(edge: .bottom),
+                            removal: .move(edge: .bottom).combined(with: .opacity)
+                        )
+                    )
+            }
+        }
+        .animation(.spring(response: 0.3, dampingFraction: isShowing ? 0.7 : 0.6, blendDuration: 40), value: isShowing)
+    }
+
+    @ViewBuilder
+    func skAlert() -> some View {
+        VStack(spacing: 36) {
+            Text(title)
+                .padding(.bottom, 36)
+                .skFont(.r18)
+
+            HStack(spacing: 16) {
+                ForEach(alertActions, id: \.id) { button in
+                    CTAButton(text: button.text, style: button.style, action: button.action)
+                }
+            }
+        }
+        .padding(14)
+        .background(Color.white)
+        .cornerRadius(20)
+    }
+}
--- App/Sources/DesignSystem/Extensions/View/SKAlertButtonType.swift
+++ App/Sources/DesignSystem/Extensions/View/SKAlertButtonType.swift
@@ -1,9 +1,18 @@
-//
-//  SKAlertButtonType.swift
-//  StackKnowledge
-//
-//  Created by 정윤서 on 2/27/24.
-//  Copyright © 2024 team.filo. All rights reserved.
-//
-
 import Foundation
+
+public struct SKAlertButtonType: Identifiable {
+    public let id: String = UUID().uuidString
+    let text: String
+    let style: CTAButton.CTAStyle
+    let action: () -> Void
+
+    public init(
+        text: String = "",
+        style: CTAButton.CTAStyle = .default,
+        action: @escaping () -> Void = {}
+    ) {
+        self.text = text
+        self.style = style
+        self.action = action
+    }
+}

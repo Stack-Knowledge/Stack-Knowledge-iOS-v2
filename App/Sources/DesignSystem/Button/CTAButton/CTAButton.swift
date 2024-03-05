c7d4c55a8d136a62436441d7418c50875f99f8ef
uuuunseo <jdbstjoo@gmail.com>
2024-03-05 16:18:13 +0900

    :sparkles: :: add CTAButton

--- App/Sources/DesignSystem/Button/CTAButton/CTAButton.swift
+++ App/Sources/DesignSystem/Button/CTAButton/CTAButton.swift
@@ -1,9 +1,31 @@
-//
-//  CTAButton.swift
-//  StackKnowledge
-//
-//  Created by 정윤서 on 2/27/24.
-//  Copyright © 2024 team.filo. All rights reserved.
-//
+import SwiftUI
 
-import Foundation
+public struct CTAButton: View {
+    var text: String
+    var style: CTAStyle
+    var action: () -> Void
+
+    public init(
+        text: String = "",
+        style: CTAStyle = .default,
+        action: @escaping () -> Void = {}
+    ) {
+        self.text = text
+        self.style = style
+        self.action = action
+    }
+
+    public var body: some View {
+        HStack {
+            Spacer()
+
+            Text(text)
+                .padding(.vertical, 18)
+
+            Spacer()
+        }
+        .buttonWrapper(action)
+        .buttonStyle(CTAButtonStyle(style: style))
+    }
+}
+
--- App/Sources/DesignSystem/Button/CTAButton/CTAButtonStyle.swift
+++ App/Sources/DesignSystem/Button/CTAButton/CTAButtonStyle.swift
@@ -1,9 +1,62 @@
-//
-//  CTAButtonStyle.swift
-//  StackKnowledge
-//
-//  Created by 정윤서 on 2/27/24.
-//  Copyright © 2024 team.filo. All rights reserved.
-//
+import SwiftUI
 
-import Foundation
+public extension CTAButton {
+    enum CTAStyle {
+        case outline
+        case `default`
+    }
+}
+
+public struct CTAButtonStyle: ButtonStyle {
+    var style: CTAButton.CTAStyle
+
+    public func makeBody(configuration: Configuration) -> some View {
+        switch style {
+        case .outline:
+            OutlineButton(configuration: configuration)
+
+        case .default:
+            DefaultButton(configuration: configuration)
+        }
+    }
+}
+
+extension CTAButtonStyle {
+    struct DefaultButton: View {
+        let configuration: ButtonStyle.Configuration
+        @Environment(\.isEnabled) var isEnabled
+        var foregroundColor: Color {
+            isEnabled ? .white : .sk(.gray(.lightgray1))
+        }
+        
+        var body: some View {
+            configuration.label
+                .skFont(.pm20)
+                .foregroundColor(foregroundColor)
+                .background(Color.sk(.main(.mainColor)))
+                .cornerRadius(8)
+        }
+    }
+}
+
+extension CTAButtonStyle {
+    struct OutlineButton: View {
+        let configuration: ButtonStyle.Configuration
+        @Environment(\.isEnabled) var isEnabled
+        var strokeColor: Color {
+            isEnabled ? .sk(.main(.mainColor)) : .clear
+        }
+
+        var body: some View {
+            configuration.label
+                .skFont(.p16)
+                .foregroundColor(Color.sk(.main(.mainColor)))
+                .background(Color.white)
+                .cornerRadius(10)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(strokeColor, lineWidth: 1)
+                }
+        }
+    }
+}

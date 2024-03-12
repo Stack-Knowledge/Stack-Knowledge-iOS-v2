a8b1a5527385b59b10b07eebbea57dea8cc0849b
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2024-03-13 00:15:48 +0900

    :sparkles: Keychain 작성
    Keychain 작성

--- App/Sources/Keychain/Keychain.swift
+++ App/Sources/Keychain/Keychain.swift
@@ -1,9 +1,37 @@
-//
-//  Keychain.swift
-//  StackKnowledge
-//
-//  Created by AnnKangHo on 3/12/24.
-//  Copyright © 2024 team.filo. All rights reserved.
-//
-
 import Foundation
+
+struct DefaultKeychain {
+    func save(key: String, value: String) {
+        let query: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: key,
+            kSecValueData: value.data(using: .utf8, allowLossyConversion: false) ?? .init()
+        ]
+        SecItemDelete(query)
+        SecItemAdd(query, nil)
+    }
+
+    func load(key: String) -> String {
+        let query: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: key,
+            kSecReturnData: kCFBooleanTrue!,
+            kSecMatchLimit: kSecMatchLimitOne
+        ]
+        var dataTypeRef: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
+        if status == errSecSuccess {
+            guard let data = dataTypeRef as? Data else { return "" }
+            return String(data: data, encoding: .utf8) ?? ""
+        }
+        return ""
+    }
+
+    func delete(key: String) {
+        let query: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: key
+        ]
+        SecItemDelete(query)
+    }
+}

7973011ef52e5e5b9443322cbf735fa63fc8d784
uuuunseo <jdbstjoo@gmail.com>
2024-05-02 13:51:42 +0900

    :sparkles: :: RequestDTO 추가

--- Service/Sources/Domain/MissionDomain/Interface/DTO/Request/CreateMissionRequestDTO.swift
+++ Service/Sources/Domain/MissionDomain/Interface/DTO/Request/CreateMissionRequestDTO.swift
@@ -1,8 +1,17 @@
-//
-//  CreateMissionRequestDTO.swift
-//  Service
-//
-//  Created by 정윤서 on 5/2/24.
-//
-
 import Foundation
+
+public struct CreateMissionRequestDTO: Encodable {
+    public let title: String
+    public let content: String
+    public let timeLimit: Int
+
+    public init(
+        title: String,
+        content: String,
+        timeLimit: Int
+    ) {
+        self.title = title
+        self.content = content
+        self.timeLimit = timeLimit
+    }
+}

b1615d2bdb7ece69856ad1bfec6ddd41d9c903b6
uuuunseo <jdbstjoo@gmail.com>
2024-06-29 22:32:08 +0900

    :sparkles: :: Impl - DTO

--- Service/Sources/Domain/ItemDomain/Sources/DTO/Response/FetchItemListResponseDTO.swift
+++ Service/Sources/Domain/ItemDomain/Sources/DTO/Response/FetchItemListResponseDTO.swift
@@ -1,8 +1,26 @@
-//
-//  FetchItemListResponseDTO.swift
-//  Service
-//
-//  Created by 정윤서 on 6/29/24.
-//
-
 import Foundation
+
+public struct FetchItemListResponseDTO: Decodable {
+    public let itemID: String
+    public let name: String
+    public let price: Int
+    public let image: String
+    
+    enum CodingKeys: String, CodingKey {
+        case itemID = "itemId"
+        case name
+        case price
+        case image
+    }
+}
+
+extension FetchItemListResponseDTO {
+    func toDomain() -> [ItemEntity] {
+        [ItemEntity(
+            itemID: self.itemID,
+            name: self.name,
+            price: self.price,
+            image: self.image
+        )]
+    }
+}

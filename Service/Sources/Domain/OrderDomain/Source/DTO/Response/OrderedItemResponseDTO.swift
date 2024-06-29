e4360a96dbcc81c9b8f12d7e417e7056e98b63be
uuuunseo <jdbstjoo@gmail.com>
2024-06-30 08:43:01 +0900

    :sparkles: :: Source - DTO

--- Service/Sources/Domain/OrderDomain/Source/DTO/Response/OrderedItemResponseDTO.swift
+++ Service/Sources/Domain/OrderDomain/Source/DTO/Response/OrderedItemResponseDTO.swift
@@ -1,8 +1,21 @@
-//
-//  OrderedItemResponseDTO.swift
-//  Service
-//
-//  Created by 정윤서 on 6/30/24.
-//
-
 import Foundation
+
+public struct OrderedItemResponseDTO: Decodable {
+    public let id: String
+    public let count: Int
+    public let price: Int
+    public let item: FetchItemResponseDTO
+    public let user: FetchUserInfoResponseDTO
+}
+
+extension OrderedItemResponseDTO {
+    func toDomain() -> [OrderedItemEntity] {
+        [OrderedItemEntity(
+            itemID: id,
+            count: count,
+            price: price,
+            item: item.toDomainEntity(),
+            user: user.toDomain()
+        )]
+    }
+}

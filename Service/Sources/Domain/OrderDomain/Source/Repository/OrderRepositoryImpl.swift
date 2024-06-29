3ac1a5866619a964d3390e9904c08242ea46a735
uuuunseo <jdbstjoo@gmail.com>
2024-06-30 08:43:32 +0900

    :sparkles: :: Impl - Repository

--- Service/Sources/Domain/OrderDomain/Source/Repository/OrderRepositoryImpl.swift
+++ Service/Sources/Domain/OrderDomain/Source/Repository/OrderRepositoryImpl.swift
@@ -1,8 +1,21 @@
-//
-//  OrderRepository.swift
-//  Service
-//
-//  Created by 정윤서 on 6/30/24.
-//
-
 import Foundation
+
+public struct OrderRepositoryImpl: OrderRepository {
+    private let remoteOrderDataSource: any RemoteOrderDataSource
+
+    public init(remoteOrderDataSource: any RemoteOrderDataSource) {
+        self.remoteOrderDataSource = remoteOrderDataSource
+    }
+
+    public func orderItem(request: OrderItemRequestDTO) async throws {
+        try await remoteOrderDataSource.orderItem(request: request)
+    }
+
+    public func fetchOrderedItem() async throws -> [OrderedItemEntity] {
+        try await remoteOrderDataSource.fetchOrderedItem()
+    }
+
+    public func changeItemState(request: ChangeItemStateRequestDTO) async throws {
+        try await remoteOrderDataSource.changeItemState(request: request)
+    }
+}

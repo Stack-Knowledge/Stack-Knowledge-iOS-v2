1de4f27c2b5d33a2a24667710e6109a47050919f
uuuunseo <jdbstjoo@gmail.com>
2024-06-30 08:41:42 +0900

    :sparkles: :: Interface - DataSource

--- Service/Sources/Domain/OrderDomain/Interface/DataSource/RemoteOrderDataSource.swift
+++ Service/Sources/Domain/OrderDomain/Interface/DataSource/RemoteOrderDataSource.swift
@@ -1,8 +1,7 @@
-//
-//  RemoteOrderDataSource.swift
-//  Service
-//
-//  Created by 정윤서 on 6/30/24.
-//
-
 import Foundation
+
+public protocol RemoteOrderDataSource {
+    func orderItem(request: OrderItemRequestDTO) async throws
+    func fetchOrderedItem() async throws -> [OrderedItemEntity]
+    func changeItemState(request: ChangeItemStateRequestDTO) async throws
+}

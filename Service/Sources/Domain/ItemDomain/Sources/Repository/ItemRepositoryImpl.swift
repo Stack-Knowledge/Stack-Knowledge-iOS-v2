b7b9ed2316d5f4c8e623ec2ac5c70291b91b02ae
uuuunseo <jdbstjoo@gmail.com>
2024-06-29 22:33:08 +0900

    :sparkles: :: Impl - Repository

--- Service/Sources/Domain/ItemDomain/Sources/Repository/ItemRepositoryImpl.swift
+++ Service/Sources/Domain/ItemDomain/Sources/Repository/ItemRepositoryImpl.swift
@@ -1,8 +1,13 @@
-//
-//  ItemRepositoryImpl.swift
-//  Service
-//
-//  Created by 정윤서 on 6/29/24.
-//
-
 import Foundation
+
+public struct ItemRepositoryImpl: ItemRepository {
+    private let remoteItemDataSource: any RemoteItemDataSource
+    
+    public init(remoteItemDataSource: any RemoteItemDataSource) {
+        self.remoteItemDataSource = remoteItemDataSource
+    }
+    
+    public func fetchItemList() async throws -> [ItemEntity] {
+        try await remoteItemDataSource.fetchItemList()
+    }
+}

21b1c49b708891fe44bf6a611c8c11681a992a6b
uuuunseo <jdbstjoo@gmail.com>
2024-06-30 08:43:18 +0900

    :sparkles: :: Impl - DataSource

--- Service/Sources/Domain/OrderDomain/Source/DataSource/OrderTarget.swift
+++ Service/Sources/Domain/OrderDomain/Source/DataSource/OrderTarget.swift
@@ -2,12 +2,52 @@ import Foundation
 
 import Alamofire
 
-public enum ItemTarget {
+public enum OrderTarget {
     case orderItem(request: OrderItemRequestDTO)
     case fetchOrderedItem
     case changeItemState(request: ChangeItemStateRequestDTO)
 }
 
-extension ItemTarget: TargetType {
-    
+extension OrderTarget: TargetType {
+    public var headerType: HTTPHeaderType {
+        switch self {
+        case .orderItem,
+             .fetchOrderedItem,
+             .changeItemState:
+            return .accessToken
+        }
+    }
+
+    public var method: HTTPMethod {
+        switch self {
+        case .orderItem:
+            return .post
+        case .fetchOrderedItem:
+            return .get
+        case .changeItemState:
+            return .patch
+        }
+    }
+
+    public var path: String {
+        switch self {
+        case .orderItem,
+             .fetchOrderedItem,
+             .changeItemState:
+            return "/order"
+        }
+    }
+
+    public var parameters: RequestParams {
+        switch self {
+        case let .orderItem(request):
+            return .requestWithBody(request)
+
+        case .fetchOrderedItem:
+            return .requestPlain
+
+        case let .changeItemState(request):
+            return .requestWithBody(request)
+        }
+    }
 }
